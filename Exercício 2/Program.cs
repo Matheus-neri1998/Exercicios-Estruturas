4f6d1060db3cacd07df468f6adc70c994d8a6047
Matheus <mthneri@gmail.com>
2020-11-18 18:05:13 -0300

    Usuario e senha

--- Exercício 2/Program.cs
+++ Exercício 2/Program.cs
@@ -6,7 +6,34 @@ namespace Exercício_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+        // var = Variável
+        // do = Faça
+        // while = Enquanto
+        // if = Se
+
+            int usuario;    // Variável usuário
+
+            int senha;     // Variável senha
+
+
+            do{
+
+            Console.WriteLine("Digite o nome de usuário? ");
+            usuario = int.Parse(Console.ReadLine());
+            // Console.WriteLine ("");
+            Console.WriteLine("Digite a sua senha ");
+            senha = int.Parse(Console.ReadLine());
+
+            if(usuario != senha) {
+                 Console.WriteLine("Senha válida");
+            }
+            }while(usuario == senha);
+            Console.WriteLine("Senha inválida");
+            Console.WriteLine("");
+            Console.WriteLine("Digite sua senha novamente");
+            senha = int.Parse(Console.ReadLine());
         }
     }
 }
