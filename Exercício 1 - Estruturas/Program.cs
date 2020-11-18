c1dd287081b7a1a4711149f7b051010e344a266a
Matheus <mthneri@gmail.com>
2020-11-18 15:16:19 -0300

    Exercicio 1 feito

---
+++ Exercício 1 - Estruturas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicios_Estruturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nota;
+
+
+           do {
+               Console.Write("Digite uma nota entre 0 e 10: ");
+               nota = int.Parse(Console.ReadLine());
+
+               if(nota <0 || nota > 10) {
+                   Console.WriteLine("Número inválido");
+               }
+
+           }while(nota < 0 || nota > 10);
+
+           Console.WriteLine("Número aceito");
+               
+           }            
+             
+        }
+    }
---
+++ Exercício 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
